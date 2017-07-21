8f7ea47e7ca1b48cc160a134e6d97399a7bbfab4
kalaider <a.kalaider@yandex.ru>
2017-07-21 15:13:49 +0300

    iobuf `write` operation tests added

--- src/test/iobuf_test.h
+++ src/test/iobuf_test.h
@@ -6,7 +6,7 @@
 typedef iobuf<byte, 3u, byte> buf_t;
 
 
-bool iobuf_test_results[64];
+bool iobuf_test_results[128];
 
 
 inline void iobuf_read_test0(byte &i)
@@ -250,10 +250,255 @@ inline void iobuf_read_test2(byte &i)
 }
 
 
+inline void iobuf_write_test0(byte &i)
+{
+    buf_t dst;
+    byte src[3];
+    byte r;
+    
+    // ------ 1 2 3 4
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 3;
+    
+    // should not do anything
+    r = iobuf_write<sp_read_any, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 0);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------ 5 6 7 8
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 0;
+    
+    // should write 2 bytes
+    r = iobuf_write<sp_read_any, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 2);
+    iobuf_test_results[i++] = (dst[0] == 0) && (dst[1] == 1) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 9 10 11 12
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 2;
+    
+    // should write 1 byte
+    r = iobuf_write<sp_read_any, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 1);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 0);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------ 13 14 15 16
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 0;
+    
+    // should write 2 bytes
+    r = iobuf_write<sp_read_any, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 2);
+    iobuf_test_results[i++] = (dst[0] == 1) && (dst[1] == 4) && (dst[2] == 0);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 17 18 19 20
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 2;
+    
+    // should write 1 byte
+    r = iobuf_write<sp_read_any, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 1);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 0) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------
+}
+
+
+inline void iobuf_write_test1(byte &i)
+{
+    buf_t dst;
+    byte src[3];
+    byte r;
+    
+    // ------ 1 2 3 4
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 3;
+    
+    // should not do anything
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 0);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------ 5 6 7 8
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 0;
+    
+    // should write 2 bytes
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 2);
+    iobuf_test_results[i++] = (dst[0] == 0) && (dst[1] == 1) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 9 10 11 12
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 2;
+    
+    // should write 0 bytes => full restriction
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 0);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 13 14 15 16
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 0;
+    
+    // should write 2 bytes
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 2);
+    iobuf_test_results[i++] = (dst[0] == 1) && (dst[1] == 4) && (dst[2] == 0);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 17 18 19 20
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 2;
+    
+    // should write 0 bytes => full restriction
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 2);
+    
+    iobuf_test_results[i++] = (r == 0);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 2);
+    
+    // ------ 21 22 23 24
+    
+         src[0] = 0;      src[1] = 1;      src[2] = 2;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 2;
+    
+    // should write 1 byte
+    r = iobuf_write<sp_read_full, lp_adopt_lock>(dst, src, (byte) 1);
+    
+    iobuf_test_results[i++] = (r == 1);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 0) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------
+}
+
+
+inline void iobuf_write_test2(byte &i)
+{
+    buf_t dst;
+    byte src;
+    byte r;
+    
+    // ------ 1 2 3 4
+    
+    src = 6;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 3;
+    
+    // should not do anything
+    r = iobuf_write<lp_adopt_lock>(dst, src);
+    
+    iobuf_test_results[i++] = (r == 0);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------ 5 6 7 8
+    
+    src = 6;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 0;
+    dst.length = 2;
+    
+    // should write 1 byte
+    r = iobuf_write<lp_adopt_lock>(dst, src);
+    
+    iobuf_test_results[i++] = (r == 1);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 4) && (dst[2] == 6);
+    iobuf_test_results[i++] = (dst.position == 0);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------ 9 10 11 12
+    
+    src = 6;
+    dst.data[0] = 3; dst.data[1] = 4; dst.data[2] = 5;
+    dst.position = 2;
+    dst.length = 2;
+    
+    // should write 1 byte
+    r = iobuf_write<lp_adopt_lock>(dst, src);
+    
+    iobuf_test_results[i++] = (r == 1);
+    iobuf_test_results[i++] = (dst[0] == 3) && (dst[1] == 6) && (dst[2] == 5);
+    iobuf_test_results[i++] = (dst.position == 2);
+    iobuf_test_results[i++] = (dst.length == 3);
+    
+    // ------
+}
+
+
 inline void iobuf_test()
 {
     byte i = 1;
+    
     iobuf_read_test0(i);
     iobuf_read_test1(i);
     iobuf_read_test2(i);
+    
+    iobuf_write_test0(i);
+    iobuf_write_test1(i);
+    iobuf_write_test2(i);
 }
