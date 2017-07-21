27a38a58887d69b08a333584c03c4e6ef17c0991
kalaider <a.kalaider@yandex.ru>
2017-07-21 15:12:45 +0300

    iobuf `write` operation implemented

--- src/include/act-common/iobuf.h
+++ src/include/act-common/iobuf.h
@@ -316,3 +316,241 @@ inline capacity_type iobuf_read
     
     return 1;
 }
+
+
+
+/**
+ *  Writes data from `src` to `dst`.
+ *
+ *  Returns `0` on failure, `1 <= X <= length` on success.
+ */
+template
+<
+    success_policy read_full,
+    locking_policy adopt_lock,
+    typename capacity_type,
+    capacity_type capacity,
+    typename storage_type
+>
+inline capacity_type iobuf_write
+(
+    iobuf < capacity_type, capacity, storage_type > & dst,
+    storage_type * src,
+    capacity_type  length
+)
+{
+    capacity_type dst_length;
+    capacity_type dst_pos; /* initially -- just position */
+    
+    if (static_cast<bool>(adopt_lock))
+    {
+        dst_pos = dst.position;
+        dst_length = dst.length;
+    }
+    else
+    {
+        lock_guard < true > guard;
+        dst_pos = dst.position;
+        dst_length = dst.length;
+    }
+    
+    /* nothing to write, just return */
+    if (dst_length == capacity) return 0;
+    
+    /* initially -- just free_space */
+    capacity_type to_write = (capacity - dst_length);
+    
+    /* the policy does not allow us to continue, just return */
+    if (read_full && (to_write < length)) return 0;
+    
+    /**
+     *  to_write = read_full ? length : min ( free_space, length )
+     */
+    if (read_full || to_write > length) to_write = length;
+    
+    capacity_type remaining = (capacity - dst_pos);
+    
+    /**
+     *  dst_pos = (position + length) mod capacity
+     */
+    if (dst_length >= remaining)
+    {
+        /**
+         *  pointers   0   w             r   c
+         *             |   |             |   |
+         *             |-|-|-|-|-|-|-|-|-|-|-|
+         *  length     |- >|             |< -|
+         *  remaining                    |< >|
+         */
+        dst_pos = (dst_length - remaining);
+    }
+    else
+    {
+        /**
+         *  pointers   0   r             w   c
+         *             |   |             |   |
+         *             |-|-|-|-|-|-|-|-|-|-|-|
+         *  length         |< - - - - - >|    
+         *  remaining      |< - - - - - - - >|
+         */
+        dst_pos = (dst_pos + dst_length);
+    }
+    
+    remaining = (capacity - dst_pos);
+    
+    if (remaining > to_write)
+    {
+        /**
+         *  pointers   0   w         |   r   c
+         *             |   |         |   |   |
+         *             |-|-|x|x|x|x|x|-|-|-|-|
+         *  to_write       |< - - - >|
+         *  free_space     |< - - - - - >|
+         *  remaining      |< - - - - - - - >|
+         */
+        memcpy(dst + dst_pos, src, to_write);
+    }
+    else
+    {
+        /**
+         *  pointers   0   |   r     w       c
+         *             |   |   |     |       |
+         *             |x|x|-|-|-|-|-|x|x|x|x|
+         *  to_write   |- >|         |< - - -|
+         *  free_space |- - - >|     |< - - -|
+         *  remaining                |< - - >|
+         */
+        
+        memcpy(dst + dst_pos, src, remaining);
+        
+        dst_pos = to_write - remaining;
+        
+        memcpy(dst, src + remaining, dst_pos);
+    }
+    
+    /**
+     *  if lock already exists (e.g. inside interrupt vector)
+     *  there is a field of optimizations - we are sure there
+     *  is no parallel dst.length decrementation
+     */
+    if (static_cast<bool>(adopt_lock))
+    {
+        dst.length = dst_length + to_write;
+    }
+    else
+    {
+        lock_guard < true > guard;
+        dst.length += to_write; /* may change since previous reading */
+    }
+    
+    return to_write;
+}
+
+
+
+/**
+ *  Same as iobuf_write < sp_read_any, lp_use_lock >
+ */
+template
+<
+    typename capacity_type,
+    capacity_type capacity,
+    typename storage_type
+>
+inline capacity_type iobuf_write_any
+(
+    iobuf < capacity_type, capacity, storage_type > & dst,
+    storage_type * src,
+    capacity_type  length
+)
+{
+    return iobuf_write < sp_read_any, lp_use_lock > (dst, src, length);
+}
+
+
+
+/**
+ *  Same as iobuf_write < > (dst, &src, 1), but optimized a bit.
+ */
+template
+<
+    locking_policy adopt_lock,
+    typename capacity_type,
+    capacity_type capacity,
+    typename storage_type
+>
+inline capacity_type iobuf_write
+(
+    iobuf < capacity_type, capacity, storage_type > & dst,
+    storage_type & src
+)
+{
+    capacity_type dst_length;
+    capacity_type dst_pos; /* initially -- just position */
+    
+    if (static_cast<bool>(adopt_lock))
+    {
+        dst_pos = dst.position;
+        dst_length = dst.length;
+    }
+    else
+    {
+        lock_guard < true > guard;
+        dst_pos = dst.position;
+        dst_length = dst.length;
+    }
+    
+    /* nothing to write, just return */
+    if (dst_length == capacity) return 0;
+    
+    /**
+     *  to_write = 1
+     */
+    
+    capacity_type remaining = (capacity - dst_pos);
+    
+    /**
+     *  dst_pos = (position + length) mod capacity
+     */
+    if (dst_length >= remaining)
+    {
+        /**
+         *  pointers   0   w             r   c
+         *             |   |             |   |
+         *             |-|-|-|-|-|-|-|-|-|-|-|
+         *  length     |- >|             |< -|
+         *  remaining                    |< >|
+         */
+        dst_pos = (dst_length - remaining);
+    }
+    else
+    {
+        /**
+         *  pointers   0   r             w   c
+         *             |   |             |   |
+         *             |-|-|-|-|-|-|-|-|-|-|-|
+         *  length         |< - - - - - >|    
+         *  remaining      |< - - - - - - - >|
+         */
+        dst_pos = (dst_pos + dst_length);
+    }
+    
+    *(dst + dst_pos) = src;
+    
+    /**
+     *  if lock already exists (e.g. inside interrupt vector)
+     *  there is a field of optimizations - we are sure there
+     *  is no parallel dst.length decrementation
+     */
+    if (static_cast<bool>(adopt_lock))
+    {
+        dst.length = dst_length + 1;
+    }
+    else
+    {
+        lock_guard < true > guard;
+        dst.length += 1; /* may change since previous reading */
+    }
+    
+    return 1;
+}
