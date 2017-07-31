a05b66c5c376fcb86962445eb6e31fa2d45d59a2
kalaider <a.kalaider@yandex.ru>
2017-07-31 14:06:22 +0300

    `size_aware_pointer` class introduced
    Unlike `array` which size is known at compile time, the class represents
    an array of runtime-specified size.
    
    The class is still an array so placed in `array` header. `array_size`
    function overloaded to support new class.

--- src/include/act-common/array.h
+++ src/include/act-common/array.h
@@ -25,10 +25,52 @@ struct array
 template
 <
     typename capacity_type,
+    typename storage_type
+>
+struct size_aware_pointer
+{
+    typedef storage_type type;
+    
+    storage_type  * data;
+    capacity_type   capacity;
+    
+    size_aware_pointer
+    (
+        storage_type  * data     = 0,
+        capacity_type   capacity = 0
+    )
+    : data(data)
+    , capacity(capacity)
+    {
+    }
+    
+    operator storage_type * ()
+    {
+        return data;
+    }
+};
+
+
+
+template
+<
+    typename capacity_type,
     capacity_type capacity,
     typename storage_type
 >
 inline capacity_type array_size(const array<capacity_type, capacity, storage_type> &)
 {
     return capacity;
+}
+
+
+
+template
+<
+    typename capacity_type,
+    typename storage_type
+>
+inline capacity_type array_size(const size_aware_pointer<capacity_type, storage_type> & arr)
+{
+    return arr.capacity;
 }
